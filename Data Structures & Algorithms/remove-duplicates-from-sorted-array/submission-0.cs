b6bd7b86ca6f1779e27bf696f81a98524545caa7
Tarek Mohamed <99132524+TarekMohame-d@users.noreply.github.com>
2026-06-28 19:34:08 +0300

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        var set = new HashSet<int>(nums);
+        int i = 0;
+
+        foreach (var num in set) {
+            nums[i] = num;
+            i++;
+        }
+
+        return set.Count;
+    }
+}
