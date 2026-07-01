16ee45b83bb945b892894495d2aecab37f3b5902
Tarek Mohamed <99132524+TarekMohame-d@users.noreply.github.com>
2026-07-01 12:46:59 +0300

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cs
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public void ReorderList(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+
+        ListNode l1 = head;
+        ListNode l2 = Reverse(Split(head));
+
+        while (l2 != null) {
+            ListNode next1 = l1.next;
+            ListNode next2 = l2.next;
+
+            l1.next = l2;
+            l2.next = next1;
+
+            l1 = next1;
+            l2 = next2;
+        }
+    }
+
+    private ListNode Split(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = slow.next;
+        slow.next = null;
+
+        return second;
+    }
+
+    private ListNode Reverse(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
